63461eb86ed3dcdd1590a0fbe6beead784201d15
Matthew Davis <matthew@mdavis.xyz>
2019-01-02 21:44:01 +1100

    more abbott content

--- pages/paragraphiser/convert.py
+++ pages/paragraphiser/convert.py
@@ -14,8 +14,8 @@ template = template.replace('{{date}}',dateStr)
 
 content = pypandoc.convert_file(content_md_fname, 'html')
 
-print("Content as html:")
-print(content)
+# print("Content as html:")
+# print(content)
 
 content = content.replace('<a href=','<a target="_blank" href=').replace('\n','\n'+' '*6)
 

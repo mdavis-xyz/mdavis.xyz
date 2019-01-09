de5874152265b4d8093c18f9ab598c62c86197b2
Matthew Davis <matthew@mdavis.xyz>
2019-01-09 22:03:22 +1100

    buttons better

--- pages/blockchain/docs/script.js
+++ pages/blockchain/docs/script.js
@@ -7,7 +7,10 @@ var currentElID = null; // won't be used until we navigate anyway
 function slide(oldElID,newElID,direction){
    slideNoHist(oldElID,newElID,direction);
    if(navHistory.length == 0) {
-      document.getElementById("back-button").setAttribute('class', 'button-back slide-in-button'); // not hidden
+      var backButton = document.getElementById("back-button");
+      backButton.classList.add('slide-in-button'); // not hidden
+      backButton.classList.remove('slide-out-button'); // not hidden
+      backButton.classList.remove('hidden'); // not hidden
    }
    navHistory.push(oldElID);
    navDirections.push(direction);
@@ -53,6 +56,8 @@ function back(){
       if (backButton == null){
          backButton = document.getElementById('back-button');
       }
-      backButton.setAttribute('class', 'button-back slide-out-button');
+      var backButton = document.getElementById("back-button")
+      backButton.classList.add('slide-out-button'); // not hidden
+      backButton.classList.remove('slide-in-button'); // not hidden
    }
 }
